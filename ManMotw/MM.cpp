adc7cd9be2514276485b7d060d10dd9cfc25db66
keqpan <safinsam@yandex.ru>
2015-06-04 23:12:53 +0300

    last

---
+++ ManMotw/MM.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+
+#include <math.h>
+
+#include <windows.h>
+
+using namespace std;
+
+bool myVecCmp(vector <int> & i, vector <int> & j) { 
+	return (i < j);
+}
+
+
+
+
+int main() {
+	fstream myfile;
+	stringstream MyStSt;
+
+	vector < vector<int> > Buffer;
+	vector <int> tmp;
+
+	char fname[20];
+	int n, l, j;
+	int bSize, curSize;
+	string str;
+	/*cin >> fname;
+	myfile.open(fname);*/
+	//system("cd");
+
+	myfile.open("example.txt");
+	if (!myfile.is_open()) {
+		cout << "Error opening file!" << endl;
+		return -1;
+	}
+
+	for (j = 0; getline(myfile, str); j++) {
+		//cout << str << '\n';
+		MyStSt.clear();
+		MyStSt.str(str);
+
+		std::string tt = MyStSt.str();
+
+		tmp.clear();
+
+		while (MyStSt >> n) {		
+			tmp.push_back(n);
+		}
+
+		Buffer.push_back(tmp);
+
+		cout << str << endl;
+		//cout << "aaf" << endl;
+
+		if (j > 20) {
+			j = 0;
+
+
+			cout << "11 < 22" << endl;
+
+			1 + 1;
+			sort(Buffer.begin(), Buffer.end(), myVecCmp);
+
+			bSize = Buffer.size();
+			for (int k = 0; k < bSize; k++) {
+				curSize = Buffer[k].size();
+				for (int z = 0; z < curSize; z++)
+					cout << Buffer[k][z] << " ";
+
+				cout << endl;
+			}
+
+			Buffer.clear();
+			getchar();
+		}
+
+	}
+	//myfile.close();
+	//myfile << "Writing this to a file.\n";
+	cout << "aaf" << endl;
+
+	getchar();
+
+	return 0;
+}
